f0c9a65552de9e030c8e32f8ef6c423c75418475
Franko-Martin09 <frankomartin23@gmail.com>
2026-01-21 11:54:56 -0300

    Actualizo archivo proncipal

--- GESTION DE ALUMNOSProyecto base.c
+++ GESTION DE ALUMNOSProyecto base.c
@@ -2,61 +2,59 @@
 #include <stdlib.h>
 struct datos
 {
-    int id;
+    int legajo;
+    int year;
     char nombre [30];
     char curso[30];
+    float nota1;
+    float nota2:
+    float nota3;
     float promedio;
     int estado;
 };
 
 
 int menu (int ingreso );//menu del programa
-void cargarDatos( );//guarda los datos en el struct alumno
+void cargarDatos( satruct datos *alumno);//guarda los datos en el struct alumno
 
 int main (){ //MAIN PRINCIPAL
+    struct datos *alumno; //puntero a struct
+    alumno=malloc(sizeof(strcut datos)); //malloc + el peso de un struct
+    int op=0;
 
-    int ingreso;
+    if(alumno == Null){//validacion de memoria asignada
+        printf("\nError al asignar memoria.\n");
+        return 1;
+    }
+
     printf("\t___GESTION DE ALUMNOS___\n\t     <Bienvenido>\n");
-
-    menu(ingreso);//LLAMA AL MENU
-    return 0;
-}
-
-int menu(int ingreso) //MENU DEL PROGRAMA- ELECCION DE OPCIONES
-{
-     struct datos alumno [4];
     do
     {
         printf("MENU:\n");
-        printf("1.ingresar alumno nuevo. \n");//ingresa y guarda los datos del alumno en un archivo de texto.
-        printf("2.Buscar alumno./s \n");// Muestra uno o todos los alumnos dependiendo del filtro que se precise: curso, edad, nombre, año etc....
-        printf("3.Editar datos del alumno.\n");// permite editar uno o varios datos juntos (ejemplo ponerles a todos una misma nota)
+        printf("1.ingresar alumno nuevo. \n");
+        printf("2.Buscar alumno./s \n");
+        printf("3.Editar datos del alumno.\n");
         printf("4.Salir.");
-        printf("\nElija una opcion >>   \n ingrese un numero del 1 al 4. \nOpcion elegida: ");
-        scanf("%d",&ingreso);
-        if(ingreso > 4 || ingreso < 1 )
+        scanf("%d",&op);
+
+        if(op > 4 || op < 1 )
         {
-            printf("\n ingrese un numero valido para acceder a una opcion del menu\n");// validar esto mas tarde***
+            printf("\n ingrese una opcion valida");
             system("cls");
         }
 
-        switch(ingreso)
-        {
-        case 1://ingresa y guarda los datos del alumno en un archivo de texto.
+        switch(op) {
+        case 1: cargarDatos(alumno);
+            break;
 
-          cargarDatos();//no recuerdo como se hacia para pasar por copia un array de struct...-_-
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            printf("\nHasta luego!");
-            break;
         }
     }
-    while(ingreso!= 4);
-};
+    while(op!= 4);
+
+return 0;
+}
+
+
 void cargarDatos()
 {
 
